eea2b74e8a43e9781187fa5a25fb79c06450338c
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2019-03-31 15:36:45 -0600

    Generating users

---
+++ User_Data_Generator.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 16 22:11:51 2019
+
+@author: Andrey
+"""
+
+import requests
+import json
+import pandas as pd
+import os
+
+def generate_csv_from_dict(data):
+    data_frame = pd.DataFrame(data)
+    data_frame.columns = ["id", "login", "html_url", "name", "email"]
+    data_frame.to_csv('C:/Users/Andrey/Desktop/GenderResearchGithubCostaRica/data/CostaRicanUsersInfo.csv', encoding='utf-8', header=True, index=False)
+    print("Finished URL generation!")
+
+os.chdir("/Users/Andrey/Desktop/GenderResearchGithubCostaRica/data")
+datos_est = pd.read_csv('CostaRicanUsers.csv')
+users = datos_est.loc[:,'usersURL']
+
+
+userName = "AndreyArguedas" # Use here your user name
+password = input("Password: ")
+
+
+API_calls = 0
+data = []
+for i in range(1, 3):
+    
+    myResponse = requests.get(users[i],auth=(userName , password), verify=True)
+
+    if(myResponse.ok):
+        userData = json.loads(myResponse.content)
+        data.append([userData["id"], userData['login'], userData['html_url'], userData['name'], userData['email']])
+        if(len(data) == 2):
+            generate_csv_from_dict(data)
+    else:
+        # If response code is not ok (200), print the resulting http error code with description
+        myResponse.raise_for_status()
--- User_URL_Generator.py
+++ User_URL_Generator.py
@@ -13,17 +13,22 @@ import pandas as pd
 url = "https://api.github.com/search/users?q=location:Costa%20Rica&per_page=100&page="
 
 usersUrl = []
-
+API_calls = 0
 
 userName = "AndreyArguedas" # Use here your user name
 password = input("Password: ")
 
+def generate_csv_from_array(data):
+    dict_data = {'usersURL': data}
+    data_frame = pd.DataFrame(dict_data)
+    data_frame.to_csv('C:/Users/Andrey/Desktop/GenderResearchGithubCostaRica/data/CostaRicanUsers.csv', encoding='utf-8', header=True, index=False)
+    print("Finished URL generation!")
 
 # It is a good practice not to hardcode the credentials. So ask the user to enter credentials at runtime
 for i in range(1, 4):
     newURL = url + str(i)
     
-    myResponse = requests.get(newURL,auth=HTTPDigestAuth(userName , password), verify=True)
+    myResponse = requests.get(newURL,auth=(userName , password), verify=True)
 
     if(myResponse.ok):
 
@@ -31,6 +36,9 @@ for i in range(1, 4):
 
         for item in jData["items"]:
             usersUrl.append(item["url"])
+        API_calls += 1
+        if(API_calls == 3):
+            generate_csv_from_array(usersUrl) 
 
     else:
         # If response code is not ok (200), print the resulting http error code with description
